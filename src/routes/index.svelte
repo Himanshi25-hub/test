d4d4e0f3dadd8edcf19921bf569504a1e55c61f6
Himanshi25_hub <himanshidixt2000@gmail.com>
2022-03-12 18:15:14 +0530

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,129 @@
+<nav class="flex bg-cyan-700 text-white pb-5 ">
+    <div class="flex cursor-pointer">
+        <img src="/static/dia1 png.png" class="w-20 h-24 pt-8 pl-7 pb-2" alt="">
+        <span class="text-4xl items-center pl-2 pt-8 font-bold ">DIAMONDBOT</span>
+    </div>
+<ul class="text-xl pl-96 px-28 pt-12 font-semibold justify-items-end">
+<div class="pl-46 flex space-x-11  ">
+    <li class="cursor-pointer hover:text-black">HOME</li>
+
+    <li class="cursor-pointer hover:text-black">PAGES</li>
+
+    <li class="cursor-pointer hover:text-black">WORK</li>
+
+    <li class="cursor-pointer hover:text-black">BLOG</li>
+
+    <li class="cursor-pointer hover:text-black">CONTACT</li>
+
+    <li class="cursor-pointer hover:text-black">STYLES</li>
+</div>
+</ul>
+</nav>
+
+<div class="h-96">
+<div>
+    <img src="designcode.jpg" class=" h-96 w-full bg-gradient-to-r from-cyan-500 to-blue-500" alt="">
+</div>
+<div class="relative -top-80 pl-96">
+    <div class="  border-2 border-white bg-white hover:bg-gray-800 opacity-90 ml-60 w-80  mb-12 p-4 text-2xl">
+        <p class=" hover:text-white p-2 text-center font-bold text-yellow-900"> 
+            DIAMOND CODE
+        </p>
+    </div>
+    <div class=" bg-black hover:bg-red-400 opacity-75 ml-72 w-56 mt-4">
+        <p class=" hover:text-white p-2 text-center text-white"> 
+            CREATE-DESIGN-CODE
+        </p>
+    </div>
+    <div>
+    <div class="pl-3">
+        <button class="h-10 w-36 bg-blue-300 hover:bg-blue-600 ml-80 mt-12 hover:text-rose-800 hover:font-semibold">PORTFOLIO</button>
+    </div>
+    </div>
+</div>
+</div>
+<div class="flex">
+    <div >
+        <img src="EYE-1.png" class="h-20 ml-48 cursor-pointer" alt="">
+        <p class="tracking-wider ml-44 text-gray-800">IN THE DETAILS</p>
+        <p class="ml-36 text-center">Lorem ipsum dolor, sit amet <br> consectetur adipisicing elit. <br> Id maxime dignissimos <br> accusamus architecto quam <br> laboriosam officiis vitae quo <br> soluta, consequatur minus <br> aspernatur provident at <br> quidem ad voluptatem, <br> esse nesciunt delectus.</p>
+        <div class="pl-48 mt-3.5 ">
+            <button class="border-2 h-10 w-24 hover:border-black  bg-blue-500 hover:bg-blue-400 hover:text-white">ABOUT</button>
+        </div>
+    </div>
+
+    <div >
+        <img src="pencil1.png" class="h-12 ml-52 mb-5 mt-4 cursor-pointer" alt="">
+        <p class="tracking-wider ml-44 text-gray-800">TIP-TOP CODING</p>
+        <p class="ml-36 text-center">Lorem ipsum dolor, sit amet <br> consectetur adipisicing elit. <br> Id maxime dignissimos <br> accusamus architecto quam <br> laboriosam officiis vitae quo <br> soluta, consequatur minus <br> aspernatur provident at <br> quidem ad voluptatem, <br> esse nesciunt delectus.</p>
+        <div class="pl-48 mt-2.5">
+            <button class="border-2 h-10 w-24 hover:border-black  bg-blue-500 hover:bg-blue-400 hover:text-white">TEAM</button>
+        </div>
+    </div>
+
+    <div >
+        <img src="daimond1.png" class="h-14 ml-52 mb-3 mt-4 cursor-pointer" alt="">
+        <p class="tracking-wider ml-44 text-gray-800">QUALITY-DESIGN</p>
+        <p class="ml-36 text-center">Lorem ipsum dolor, sit amet <br> consectetur adipisicing elit. <br> Id maxime dignissimos <br> accusamus architecto quam <br> laboriosam officiis vitae quo <br> soluta, consequatur minus <br> aspernatur provident at <br> quidem ad voluptatem, <br> esse nesciunt delectus.</p>
+        <div class="pl-48 mt-2.5">
+            <button class="h-10 w-24 border-2 hover:border-black  bg-blue-500 hover:bg-blue-400 hover:text-white">WORK</button>
+        </div>
+    </div>
+
+    <div >
+        <img src="thumb1.png" class="h-14 ml-52 mb-3 mt-4 cursor-pointer" alt="">
+        <p class="tracking-wider ml-44 text-gray-800">HAPPY CLIENTS</p>
+        <p class="ml-36 text-center">Lorem ipsum dolor, sit amet <br> consectetur adipisicing elit. <br> Id maxime dignissimos <br> accusamus architecto quam <br> laboriosam officiis vitae quo <br> soluta, consequatur minus <br> aspernatur provident at <br> quidem ad voluptatem, <br> esse nesciunt delectus.</p>
+        <div class="pl-48 mt-2.5 ">
+            <button class="border-2 hover:border-black h-10 w-24 bg-blue-500 hover:bg-blue-400 hover:text-white">QUOTE</button>
+        </div>
+    </div>
+</div>
+<div class="ml-56 mt-20">
+<div class=" border-8 border-t-gray-600 hover:border-t-white hover:border-b-white border-b-gray-600 bg-white-800 opacity-90 ml-96 w-96 p-6 font-bold text-xl hover:border-r-gray-600 hover:border-l-gray-600 ">
+    <p class="text-black p-2 text-center hover:text-red-900 "> 
+        CODE PRACTICE PLATFORMS
+    </p>
+</div>
+</div>
+<div class="flex mt-20 gap-x-8 pb-5 ">
+<div class="pr-8 pl-20 border-r-8 border-r-gray-600">
+    <img src="Hacker rank1.png" class="h-42 mt-20 cursor-pointer" alt="">
+</div>
+<div class="pr-8 pl-8 border-r-8 border-r-gray-600">
+    <img src="codechef.png" class="mt-2 mr-9 pl-6 pr-8 cursor-pointer" alt="">
+</div>
+<div class="pr-8 pl-8 border-r-8 border-r-gray-600">
+    <img src="leet code.png" class="mt-14 mr-9 pl-9 pr-6 cursor-pointer" alt="">
+</div>
+<div class="pr-8 pl-8 border-r-8 border-r-gray-600">
+    <img src="coderbyte.png" class="mt-12 mr-16 cursor-pointer " alt="">
+</div>
+<div class="pr-8 pl-8 h-52 w-3/12">
+    <img src="sololearn1.png" class="mt-20" alt="">
+</div>
+</div>
+<footer>
+    <div class="flex bg-cyan-700 pt-10">
+        <div>
+            <p class="pl-44 font-semibold relative -top-3.5 cursor-pointer">
+                &copy; DAIMONDBOT
+            </p>
+        </div>
+        <div class="flex pl-96 relative -top-3.5 cursor-pointer ">
+            <div class="pl-96 flex ">
+            <div>
+            <img src="insta.jpg" class="h-8 pr-3" alt="">
+            </div>
+
+            <div>
+            <img src="fb.png" class="h-8 pr-3" alt="">
+            </div>
+
+            <div>
+            <img src="twitter.png" class="h-8 w-8" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
